7b94edaf804729012da7b6804c4ba0dc66accf1f
Trevor Suarez <rican7@gmail.com>
2018-11-18 05:14:59 -0700

    More informative message when registering service workers

--- app/scripts/service-worker-registration.js
+++ app/scripts/service-worker-registration.js
@@ -2,10 +2,10 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('service-worker.js').then(function() {
-            console.log('Service worker registered!');
+        navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+            console.log('Service worker registered with scope: ', registration.scope);
         }).catch(function(e) {
-            console.error('Failed to register service worker', e);
+            console.error('Failed to register service worker: ', e);
         });
     });
 }
