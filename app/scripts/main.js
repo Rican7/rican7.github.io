cddf67daaecde75b11ab63d5229964c0ed159ed3
Trevor Suarez <rican7@gmail.com>
2019-12-03 19:34:46 -0700

    Modernizing the JS a bit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,16 @@
-(function() {
+(() => {
     console.log('Trevor says hi 👋');
 
     const heroCardContainerElement = document.querySelector('.hero-card-container');
 
-    heroCardContainerElement.querySelectorAll('img').forEach(function (element) {
+    heroCardContainerElement.querySelectorAll('img').forEach(element => {
         // Eagerly decode the images in the card container, to increase the
         // performance of the first animation
         element.decode();
     })
 
     // Add a click-listener for handling the card flipping
-    heroCardContainerElement.addEventListener('click', function () {
+    heroCardContainerElement.addEventListener('click', () => {
         let flipping = heroCardContainerElement.classList.toggle('is-flipped');
 
         heroCardContainerElement.classList.toggle('is-unflipped', !flipping);
