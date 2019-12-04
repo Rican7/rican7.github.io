0a6508b953c9ca102678ae7e6cb74588b9aab4af
Trevor Suarez <rican7@gmail.com>
2019-12-03 19:12:36 -0700

    Adding a "hidden" alternative profile-photo with a card-flip animation to reveal it

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('Trevor says hi 👋');
+(function() {
+    console.log('Trevor says hi 👋');
+
+    const heroCardContainerElement = document.querySelector('.hero-card-container');
+
+    heroCardContainerElement.addEventListener('click', function () {
+        let flipping = heroCardContainerElement.classList.toggle('is-flipped');
+
+        heroCardContainerElement.classList.toggle('is-unflipped', !flipping);
+    });
+})()
