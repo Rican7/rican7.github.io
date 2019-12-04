0968574f5d79a816ef492672d5e41397f936ea3f
Trevor Suarez <rican7@gmail.com>
2019-12-03 22:19:54 -0700

    Updating JS files' style based on `prettier` fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,26 @@
 (() => {
-    console.log('Trevor says hi 👋');
+  console.log('Trevor says hi 👋');
 
-    const heroCardContainerElement = document.querySelector('.hero-card-container');
+  const heroCardContainerElement = document.querySelector(
+    '.hero-card-container'
+  );
 
-    heroCardContainerElement.querySelectorAll('img').forEach(element => {
-        // Eagerly decode the images in the card container, to increase the
-        // performance of the first animation
-        if (typeof element.decode === 'function') {
-            element.decode().then(() => {
-                // Eagerly calculate the element's bounds, before the animation
-                // has to calculate them "just-in-time"
-                element.getBoundingClientRect();
-            });
-        }
-    })
+  heroCardContainerElement.querySelectorAll('img').forEach(element => {
+    // Eagerly decode the images in the card container, to increase the
+    // performance of the first animation
+    if (typeof element.decode === 'function') {
+      element.decode().then(() => {
+        // Eagerly calculate the element's bounds, before the animation
+        // has to calculate them "just-in-time"
+        element.getBoundingClientRect();
+      });
+    }
+  });
 
-    // Add a click-listener for handling the card flipping
-    heroCardContainerElement.addEventListener('click', () => {
-        let flipping = heroCardContainerElement.classList.toggle('is-flipped');
+  // Add a click-listener for handling the card flipping
+  heroCardContainerElement.addEventListener('click', () => {
+    let flipping = heroCardContainerElement.classList.toggle('is-flipped');
 
-        heroCardContainerElement.classList.toggle('is-unflipped', !flipping);
-    });
-})()
+    heroCardContainerElement.classList.toggle('is-unflipped', !flipping);
+  });
+})();
--- app/scripts/service-worker-registration.js
+++ app/scripts/service-worker-registration.js
@@ -1,11 +1,17 @@
 'use strict';
 
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-            console.log('Service worker registered with scope: ', registration.scope);
-        }).catch(function(e) {
-            console.error('Failed to register service worker: ', e);
-        });
-    });
+  window.addEventListener('load', function() {
+    navigator.serviceWorker
+      .register('service-worker.js')
+      .then(function(registration) {
+        console.log(
+          'Service worker registered with scope: ',
+          registration.scope
+        );
+      })
+      .catch(function(e) {
+        console.error('Failed to register service worker: ', e);
+      });
+  });
 }
