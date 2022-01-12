fac5f0ccbbc700a926c3fd81e75572294fa63370
Trevor Suarez <rican7@gmail.com>
2022-01-12 00:49:59 -0700

    Updating the gulpfile for sass library changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
 const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
 const del = require('del');
 const injectSvg = require('gulp-inject-svg');
 const deploy = require('gulp-gh-pages');
@@ -32,80 +33,91 @@ function writeServiceWorkerFile(rootDir, handleFetch, callback) {
       rootDir + '/images/**.*',
       rootDir + '/styles/**.css',
       rootDir + '/scripts/**.js',
-      rootDir + '/**.html'
+      rootDir + '/**.html',
     ],
     stripPrefix: rootDir + '/',
     // verbose defaults to false, but for the purposes of this demo, log more.
-    verbose: true
+    verbose: true,
   };
 
   swPrecache.write(path.join(rootDir, 'service-worker.js'), config, callback);
 }
 
-gulp.task('generate-service-worker-dev', function(cb) {
+gulp.task('generate-service-worker-dev', function (cb) {
   writeServiceWorkerFile(DEV_DIR, false, cb);
 });
 
-gulp.task('generate-service-worker-dist', function(cb) {
+gulp.task('generate-service-worker-dist', function (cb) {
   writeServiceWorkerFile(DIST_DIR, true, cb);
 });
 
 function appInjectSvgs() {
   return injectSvg({
-    base: 'app'
-  })
+    base: 'app',
+  });
 }
 
 gulp.task('svgs', () => {
-  return gulp.src('app/*.html')
-    .pipe(appInjectSvgs())
-    .pipe(gulp.dest('.tmp'));
+  return gulp.src('app/*.html').pipe(appInjectSvgs()).pipe(gulp.dest('.tmp'));
 });
 
 gulp.task('styles', () => {
-  return gulp.src('app/styles/*.scss')
+  return gulp
+    .src('app/styles/*.scss')
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
-    .pipe($.sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
+    .pipe(
+      sass
+        .sync({
+          outputStyle: 'expanded',
+          precision: 10,
+          includePaths: ['.'],
+        })
+        .on('error', sass.logError)
+    )
     .pipe($.autoprefixer())
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest('.tmp/styles'))
-    .pipe(reload({stream: true}));
+    .pipe(reload({ stream: true }));
 });
 
 gulp.task('scripts', () => {
-  return gulp.src('app/scripts/**/*.js')
+  return gulp
+    .src('app/scripts/**/*.js')
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
-    .pipe(prettier({
-      singleQuote: true
-    }))
-    .pipe($.babel({
-      presets: ['@babel/env']
-    }))
+    .pipe(
+      prettier({
+        singleQuote: true,
+      })
+    )
+    .pipe(
+      $.babel({
+        presets: ['@babel/env'],
+      })
+    )
     .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest('.tmp/scripts'))
-    .pipe(reload({stream: true}));
+    .pipe(reload({ stream: true }));
 });
 
 function lint(files, options) {
-  return gulp.src(files)
-    .pipe(reload({stream: true, once: true}))
-    .pipe(function () {
-      if (options.fix) {
-        return prettier({
-          singleQuote: true
-        });
-      }
+  return gulp
+    .src(files)
+    .pipe(reload({ stream: true, once: true }))
+    .pipe(
+      (function () {
+        if (options.fix) {
+          return prettier({
+            singleQuote: true,
+          });
+        }
 
-      return prettier.check({
-        singleQuote: true
-      });
-    }())
+        return prettier.check({
+          singleQuote: true,
+        });
+      })()
+    )
     .pipe($.eslint(options))
     .pipe($.eslint.format())
     .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
@@ -113,122 +125,147 @@ function lint(files, options) {
 
 gulp.task('lint', () => {
   return lint('app/scripts/**/*.js', {
-    fix: true
-  })
-    .pipe(gulp.dest('app/scripts'));
+    fix: true,
+  }).pipe(gulp.dest('app/scripts'));
 });
 gulp.task('lint:test', () => {
   return lint('test/spec/**/*.js', {
     fix: true,
     env: {
-      mocha: true
-    }
-  })
-    .pipe(gulp.dest('test/spec/**/*.js'));
+      mocha: true,
+    },
+  }).pipe(gulp.dest('test/spec/**/*.js'));
 });
 
-gulp.task('html', gulp.series('styles', 'scripts', () => {
-  return gulp.src('app/*.html')
-    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if('*.js', uglify()))
-    .pipe($.if('*.css', $.cssnano({safe: true, autoprefixer: false})))
-    .pipe($.if('*.html', appInjectSvgs()))
-    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
-    .pipe(gulp.dest('dist'));
-}));
+gulp.task(
+  'html',
+  gulp.series('styles', 'scripts', () => {
+    return gulp
+      .src('app/*.html')
+      .pipe($.useref({ searchPath: ['.tmp', 'app', '.'] }))
+      .pipe($.if('*.js', uglify()))
+      .pipe($.if('*.css', $.cssnano({ safe: true, autoprefixer: false })))
+      .pipe($.if('*.html', appInjectSvgs()))
+      .pipe($.if('*.html', $.htmlmin({ collapseWhitespace: true })))
+      .pipe(gulp.dest('dist'));
+  })
+);
 
 gulp.task('images', () => {
-  return gulp.src([
-      'app/images/**/*',
-      '.tmp/images/**/*'
-    ])
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
-      svgoPlugins: [{cleanupIDs: false}]
-    })))
+  return gulp
+    .src(['app/images/**/*', '.tmp/images/**/*'])
+    .pipe(
+      $.cache(
+        $.imagemin({
+          progressive: true,
+          interlaced: true,
+          // don't remove IDs from SVGs, they are often used
+          // as hooks for embedding and styling
+          svgoPlugins: [{ cleanupIDs: false }],
+        })
+      )
+    )
     .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('extras', () => {
-  return gulp.src([
-    'app/*',
-    '!app/*.html',
-    'app/docs/*',
-  ], {
-    dot: true,
-    nodir: true,
-    base: './app/'
-  }).pipe(gulp.dest('dist'));
+  return gulp
+    .src(['app/*', '!app/*.html', 'app/docs/*'], {
+      dot: true,
+      nodir: true,
+      base: './app/',
+    })
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
-gulp.task('serve', gulp.series('styles', 'scripts', 'svgs', 'generate-service-worker-dev', () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['.tmp', 'app'],
-      routes: {
-      }
-    },
-    ghostMode: false
-  });
+gulp.task(
+  'serve',
+  gulp.series(
+    'styles',
+    'scripts',
+    'svgs',
+    'generate-service-worker-dev',
+    () => {
+      browserSync({
+        notify: false,
+        port: 9000,
+        server: {
+          baseDir: ['.tmp', 'app'],
+          routes: {},
+        },
+        ghostMode: false,
+      });
 
-  gulp.watch([
-    '.tmp/*.html',
-    'app/*.html',
-    'app/images/**/*',
-  ]).on('change', reload);
+      gulp
+        .watch(['.tmp/*.html', 'app/*.html', 'app/images/**/*'])
+        .on('change', reload);
 
-  gulp.watch('app/styles/**/*.scss', gulp.series('styles'));
-  gulp.watch('app/scripts/**/*.js', gulp.series('scripts'));
-  gulp.watch('app/*.html', gulp.series('svgs'));
-}));
+      gulp.watch('app/styles/**/*.scss', gulp.series('styles'));
+      gulp.watch('app/scripts/**/*.js', gulp.series('scripts'));
+      gulp.watch('app/*.html', gulp.series('svgs'));
+    }
+  )
+);
 
 gulp.task('serve:dist', () => {
   browserSync({
     notify: false,
     port: 9000,
     server: {
-      baseDir: ['dist']
+      baseDir: ['dist'],
     },
-    ghostMode: false
+    ghostMode: false,
   });
 });
 
-gulp.task('serve:test', gulp.parallel('scripts', () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    ui: false,
-    server: {
-      baseDir: 'test',
-      routes: {
-        '/scripts': '.tmp/scripts',
-      }
-    },
-    ghostMode: false
-  });
+gulp.task(
+  'serve:test',
+  gulp.parallel('scripts', () => {
+    browserSync({
+      notify: false,
+      port: 9000,
+      ui: false,
+      server: {
+        baseDir: 'test',
+        routes: {
+          '/scripts': '.tmp/scripts',
+        },
+      },
+      ghostMode: false,
+    });
 
-  gulp.watch('app/scripts/**/*.js', gulp.series('scripts'));
-  gulp.watch('test/spec/**/*.js').on('change', reload);
-  gulp.watch('test/spec/**/*.js', gulp.series('lint:test'));
-}));
+    gulp.watch('app/scripts/**/*.js', gulp.series('scripts'));
+    gulp.watch('test/spec/**/*.js').on('change', reload);
+    gulp.watch('test/spec/**/*.js', gulp.series('lint:test'));
+  })
+);
 
-gulp.task('build', gulp.series('lint', 'html', 'images', 'extras', 'generate-service-worker-dist', () => {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
-}));
+gulp.task(
+  'build',
+  gulp.series(
+    'lint',
+    'html',
+    'images',
+    'extras',
+    'generate-service-worker-dist',
+    () => {
+      return gulp.src('dist/**/*').pipe($.size({ title: 'build', gzip: true }));
+    }
+  )
+);
 
-gulp.task('deploy', gulp.series('build', () => {
-  return gulp.src('dist/**/*')
-    .pipe(deploy({
-      remoteUrl: 'https://github.com/Rican7/rican7.github.io.git',
-      branch: 'master'
-    }));
-}));
+gulp.task(
+  'deploy',
+  gulp.series('build', () => {
+    return gulp.src('dist/**/*').pipe(
+      deploy({
+        remoteUrl: 'https://github.com/Rican7/rican7.github.io.git',
+        branch: 'master',
+      })
+    );
+  })
+);
 
 gulp.task('default', gulp.series('clean', 'build'));
